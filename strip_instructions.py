8bb373be5f888d2996500db1adec5f359d520393
Catarina <catarina.c.ubach@gmail.com>
2015-07-01 22:54:22 -0400

    Update strip_instructions.py
    Creating a backup to scripForImaging.py if infilename == oufilename.
    Change the input, so that the user can chose the name of the input/output file.

--- strip_instructions.py
+++ strip_instructions.py
@@ -1,14 +1,23 @@
-def strip_instructions(infilename='scriptForImaging_template.py', outfilename='scriptForImaging.py'):
+def strip_instructions(infilename, outfilename):
 
     """
     Remove the instructions to the data imager from the scriptForImaging_template.py
     """
     
-
+    import shutil
+    
     if infilename == outfilename:
-        print "set infilename and outfilename to different values!"
-        return
-
+        # TESTING -- CU
+        print "infilename equals outfilename!"
+        print "Creating a backup of scripForImaging.py"
+        backup=infilename+'.backup.py'
+        shutil.copyfile(infilename,backup)
+        
+        tempfilename = infilename+'.temp'
+        tempfile = shutil.move(infilename,tempfilename)
+        
+        infilename=temfile
+    
     infile = open(infilename,'r')
     outfile = open(outfilename, 'w')
 
@@ -20,4 +29,16 @@ def strip_instructions(infilename='scriptForImaging_template.py', outfilename='s
 
     infile.close()
     outfile.close()
+    
+help(strip_instructions)
+strip_instructions()
+    This function runs within CASA.
+    Takes in two string variables infilename and outfilename.
+    It is perferable that the file names are not the same.
+    
+    Example: From within CASA
+        execfile('strip_instructions')
+        strip_instructions('scriptForImaging_template.py','scriptForImaging.py') 
+        
+
     
